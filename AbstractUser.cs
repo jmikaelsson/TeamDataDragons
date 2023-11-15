536d3a05504199a9a8c37ccce8b22a1469ea9731
Jana Johansson <Janajohansson00@hotmail.com>
2023-11-15 15:18:50 +0100

    Created a AbstractUser class.

---
+++ AbstractUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamDataDragons
+{
+    public abstract class AbstractUser
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public int PersonalNumber { get; set; }
+        public bool IsAdmin { get; set; }
+
+        public AbstractUser(string name, int id, int personalnumber, bool isadmin)
+        {
+            Name = name;
+            ID = id;
+            PersonalNumber = personalnumber;
+            IsAdmin = isadmin;
+
+        }
+        public abstract void PrintInfo(); 
+        
+    }
+}
