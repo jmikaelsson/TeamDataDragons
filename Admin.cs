7087281839b02efacd6f91a8f87fbeb84b12bdea
viktoria wallström <viktoria-wallstrom@hotmail.com>
2023-11-16 09:09:15 +0100

    Created SystemOwner class and methods in admin class /Viktoria

--- Admin.cs
+++ Admin.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -34,6 +35,78 @@ namespace TeamDataDragons
         public List<string> GetCustomers()
         {
             return customers;
+        }
+
+        //Method for unlock user
+        public void UnlockUser(string userName)
+        {
+            if (administrators.Contains(userName))
+            {
+                Console.WriteLine("");
+            }
+
+        }
+
+        //Method for the admin menu
+        private void AdministratorMenu()
+        {
+            Admin adminManager = new Admin();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("*** Administrator Menu ***");
+                Console.WriteLine("1. Add a new administrator\n2. View administrators information\n" +
+                    "3. Add a new customer\n4. View customers information\n 5. Update Exchange Rate\n6. Unlock User\n7. Log out");
+
+                //Create a variable for the administrators choice
+                string adminSelection = Console.ReadLine();
+
+                //A switch that goes to the case that admin selects
+                switch (adminSelection)
+                {
+                    case "1":
+                        Console.WriteLine("Enter administrator name: ");
+                        string administratorName = Console.ReadLine();
+                        adminManager.AddAdministrator(administratorName);
+                        Console.WriteLine("Administrator added!");
+                        break;
+                    case "2":
+                        Console.WriteLine("Administrators:");
+                        foreach (var admin in adminManager.GetAdministrators())
+                        {
+                            Console.WriteLine(admin);
+                        }
+                        break;
+                    case "3":
+                        Console.Write("Enter customer name: ");
+                        string customerName = Console.ReadLine();
+                        adminManager.AddCustomer(customerName);
+                        Console.WriteLine("Customer added!");
+                        break;
+                    case "4":
+                        Console.WriteLine("Customers:");
+                        foreach (var customer in adminManager.GetCustomers())
+                        {
+                            Console.WriteLine(customer);
+                        }
+                        break;
+                    case "5":
+                        Console.WriteLine("Exchange rate");
+                        break;
+                    case "6":
+                        Console.WriteLine("Unlock user");
+                        break;
+                    case "7":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Try again.");
+                        break;
+                }
+
+            } 
+
         }
 
     }
---
+++ SystemOwner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamDataDragons
+{
+    public abstract class SystemOwner
+    {
+
+        int attempts = 3;
+
+        public void UserMenu()
+        {
+            Console.WriteLine("1. Log in as administrator\n2. Log in as costumer\nSelect option (1-2)");
+
+            string userSelection = Console.ReadLine();
+
+            if (userSelection == "1")
+            {
+                AdminLogin();
+            }
+            else if (userSelection == "2")
+            {
+                CustomerLogin();
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Try again.");
+                UserMenu();
+            }
+        }
+    }
+}
