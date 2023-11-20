03ff2953c99a3e65e5172c5042d0f3de948ff950
Morgan <144250478+IceDropp@users.noreply.github.com>
2023-11-20 10:12:56 +0100

    riktig versionen av account

--- Account.cs
+++ Account.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using System;
-
 namespace TeamDataDragons
 {
     public enum CurrencyType
@@ -14,48 +12,68 @@ namespace TeamDataDragons
         Dollar
     }
 
-    public class Account
+    public enum AccountType
     {
-        // Bankkontoattribut
-        public double Balance { get; set; }
-        public string BankAccountNumber { get; set; }
-        public Currency UserCurrency { get; private set; }
+        Savings,
+        Salary
+    }
 
-        // Konstruktor
-        public Account(string bankAccountNumber, double initialBalance, CurrencyType currencyType)
+    public class AccountCurrency
+    {
+        public double Sek { get; set; }
+        public double Dollar { get; set; }
+        public double ExchangeRate { get; private set; }
+
+        public AccountCurrency(double sek, double dollar)
         {
-            BankAccountNumber = bankAccountNumber;
-            UserCurrency = new Currency(0, 0); // initialize with 0 balance
-            SetInitialBalance(initialBalance, currencyType);
+            Sek = sek;
+            Dollar = dollar;
+            UpdateExchangeRate(); // Initialize exchange rate
         }
 
- 
-        // ... (existing code)
+        public void UpdateExchangeRate()
+        {
+            // Your logic to update the exchange rate goes here
+            // For simplicity, let's set it to a fixed value for now
+            ExchangeRate = 1.0;
+        }
+    }
 
-        // Metod för att öppna ett nytt konto
-        public static void AddNewAccount()
+    public class Account
+    {
+        public double Balance { get; private set; }
+        public string BankAccountNumber { get; private set; }
+        public AccountCurrency UserCurrency { get; private set; }
+        public AccountType Type { get; private set; }
 
-        // Metod för att beräkna ränta och visa resultatet
+        public Account(string bankAccountNumber, double initialBalance, CurrencyType currencyType, AccountType accountType)
+        {
+            BankAccountNumber = bankAccountNumber;
+            UserCurrency = new AccountCurrency(0, 0);
+            SetInitialBalance(initialBalance, currencyType);
+            Type = accountType;
+        }
+
         public void Interest()
         {
-            double interestRate = 0.05; // Exempelräntesats på 5%
+            double interestRate = 0.05;
             double interest = Balance * interestRate;
             Console.WriteLine($"For account {BankAccountNumber}, the interest will be: {interest}");
         }
 
-        // Metod för att öppna ett nytt konto
         public static Account AddNewAccount()
-
         {
             Console.WriteLine("Enter the initial balance for the new account:");
+
             if (!double.TryParse(Console.ReadLine(), out double initialBalance))
             {
                 Console.WriteLine("Invalid input for initial balance.");
-                return;
+                // Return a default Account instance
+                return new Account("", 0, CurrencyType.SEK, AccountType.Savings);
             }
 
             Console.WriteLine("Choose a currency: Enter 'SEK' for Swedish Krona or 'Dollar' for US Dollar");
-            string currencyChoice = Console.ReadLine()?.ToLower() ?? ""; // Handling null reference here
+            string currencyChoice = Console.ReadLine()?.ToLower() ?? "";
 
             CurrencyType chosenCurrency;
             switch (currencyChoice)
@@ -72,51 +90,103 @@ namespace TeamDataDragons
                     break;
             }
 
-            // Create an instance of the Account class
-            Account newAccount = new Account("", 0, CurrencyType.SEK);
+            Console.WriteLine("Choose the account type: Enter 'Savings' or 'Salary'");
+            string accountTypeChoice = Console.ReadLine()?.ToLower() ?? "";
 
-            // Here the chosen currency is set to the initial balance
+            AccountType chosenAccountType;
+            switch (accountTypeChoice)
+            {
+                case "savings":
+                    chosenAccountType = AccountType.Savings;
+                    break;
+                case "salary":
+                    chosenAccountType = AccountType.Salary;
+                    break;
+                default:
+                    Console.WriteLine("Invalid account type. Defaulting to Savings.");
+                    chosenAccountType = AccountType.Savings;
+                    break;
+            }
 
+            Account newAccount = new Account("", 0, CurrencyType.SEK, AccountType.Savings);
             newAccount.SetInitialBalance(initialBalance, chosenCurrency);
-
-            // Generate a unique 8-digit random bank account number
-            string generatedAccountNumber = GenerateRandomAccountNumber();
-
-            Console.WriteLine($"New account {generatedAccountNumber} opened with initial balance: {initialBalance} {chosenCurrency}");
-
-            var newAccount = new Account("", 0, CurrencyType.SEK);
-            newAccount.SetInitialBalance(initialBalance, chosenCurrency);
-
-            // Generate a unique 8-digit random bank account number
             newAccount.GenerateRandomAccountNumber();
+            newAccount.Type = chosenAccountType;
 
-            Console.WriteLine($"New account {newAccount.BankAccountNumber} opened with initial balance: {initialBalance} {chosenCurrency}");
+            Console.WriteLine($"New {chosenAccountType} account {newAccount.BankAccountNumber} opened with initial balance: {initialBalance} {chosenCurrency}");
 
             return newAccount;
-
         }
 
-        // Helper method to set the initial balance based on the chosen currency
-        private void SetInitialBalance(double initialBalance, CurrencyType currencyType)
+        public void TransferMoneyBetweenAccounts(Account recipientAccount, double amount)
         {
-            if (UserCurrency == null)
+            if (amount <= 0)
             {
-                Console.WriteLine("Error: UserCurrency is null.");
+                Console.WriteLine("Invalid transfer amount.");
                 return;
             }
 
-            UserCurrency = new Currency(0, 0); // initialize with 0 balance
-            UserCurrency.UpdateExchangeRate(); // ask for initial exchange rate
+            if (Balance < amount)
+            {
+                Console.WriteLine("Insufficient funds for the transfer.");
+                return;
+            }
 
-            // Set the balance based on the chosen currency
+            // Transfer money
+            Balance -= amount;
+            recipientAccount.Balance += amount;
+
+            Console.WriteLine($"Transfer successful. New balance for {BankAccountNumber}: {Balance}, New balance for {recipientAccount.BankAccountNumber}: {recipientAccount.Balance}");
+        }
+
+        public static void ShowMenu(List<Account> accounts)
+        {
+            Console.WriteLine("Accounts:");
+            for (int i = 0; i < accounts.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {accounts[i].BankAccountNumber} ({accounts[i].Type})");
+            }
+
+            Console.WriteLine("Choose the source account (enter the corresponding number):");
+            if (!int.TryParse(Console.ReadLine(), out int sourceIndex) || sourceIndex < 1 || sourceIndex > accounts.Count)
+            {
+                Console.WriteLine("Invalid input. Aborting operation.");
+                return;
+            }
+
+            Console.WriteLine("Choose the destination account (enter the corresponding number):");
+            if (!int.TryParse(Console.ReadLine(), out int destinationIndex) || destinationIndex < 1 || destinationIndex > accounts.Count || destinationIndex == sourceIndex)
+            {
+                Console.WriteLine("Invalid input. Aborting operation.");
+                return;
+            }
+
+            Console.WriteLine("Enter the amount to transfer:");
+            if (!double.TryParse(Console.ReadLine(), out double transferAmount) || transferAmount <= 0)
+            {
+                Console.WriteLine("Invalid transfer amount. Aborting operation.");
+                return;
+            }
+
+            Account sourceAccount = accounts[sourceIndex - 1];
+            Account destinationAccount = accounts[destinationIndex - 1];
+
+            sourceAccount.TransferMoneyBetweenAccounts(destinationAccount, transferAmount);
+        }
+
+        private void SetInitialBalance(double initialBalance, CurrencyType currencyType)
+        {
+            UserCurrency = new AccountCurrency(0, 0);
+            UserCurrency.UpdateExchangeRate();
+
             if (currencyType == CurrencyType.SEK)
             {
                 UserCurrency.Sek = initialBalance;
+                Balance = initialBalance;
             }
             else
             {
-
-                if (UserCurrency.ExchangeRate != 0) // Handling potential divide by zero
+                if (UserCurrency.ExchangeRate != 0)
                 {
                     Balance = initialBalance / UserCurrency.ExchangeRate;
                 }
@@ -126,12 +196,10 @@ namespace TeamDataDragons
                 }
 
                 UserCurrency.Dollar = initialBalance;
-
             }
         }
 
-        // Helper method to generate a unique 8-digit random bank account number
-        private static string GenerateRandomAccountNumber()
+        private void GenerateRandomAccountNumber()
         {
             Random random = new Random();
             bool isUnique = false;
@@ -142,13 +210,12 @@ namespace TeamDataDragons
                 int randomNumber = random.Next(10000000, 99999999);
                 generatedAccountNumber = randomNumber.ToString();
 
-                // Check if the generated number is unique (you need to implement this logic)
+                // Check if the generated number is unique
                 // For simplicity, assuming it's always unique in this example
                 isUnique = true;
             }
 
-            return generatedAccountNumber;
+            BankAccountNumber = generatedAccountNumber;
         }
     }
 }
-
